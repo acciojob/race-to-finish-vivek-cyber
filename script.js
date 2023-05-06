de12232b6ab7de02476382acdfbd6bfe57394eb1
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-05-06 22:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+const promises = [];
+
+for (let i = 0; i < 5; i++) {
+  promises.push(new Promise((resolve) => {
+    const randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime * 1000);
+  }));
+}
+
+Promise.any(promises).then((result) => {
+  document.getElementById("output").textContent = result;
+});
